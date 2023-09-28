e1f61901f29b9c647c4de5445a3a65d3560405e8
Cristian S <140106882+lasuscjs@users.noreply.github.com>
2023-09-28 13:38:49 -0700

    Update raw2read.py
    Reverted to original

--- adaptllm/raw2read.py
+++ adaptllm/raw2read.py
@@ -7,8 +7,7 @@ from utils.read import TYPES, type_map, get_max_workers
 from pysbd import Segmenter
 import copy
 
-def search(entry, overall_cls):
-# def search(entry) this is OLD code pre CS change:
+def search(entry):
     # collect text title, which is the 1st sentence in the raw text
     title = entry['text'].split('\n')[0]
     context_wo_title = '\n'.join(entry['text'].split('\n')[1:]) 
@@ -86,8 +85,7 @@ if __name__ == "__main__":
     segmenter = Segmenter(language='en',clean=False)
 
     print('transferring raw texts into reading comprehension...')
-    #read_compre =list(process_map(search, raw_texts, max_workers=max_workers, chunksize=8192))  this is OLD code pre CS change
-    read_compre = list(process_map(lambda entry: search(entry, overall_cls), raw_texts, max_workers=max_workers, chunksize=8192))
+    read_compre =list(process_map(search, raw_texts, max_workers=max_workers, chunksize=8192))
 
     print('saving reading comprehension texts...')
     # sort by text_id to align with the order of raw texts
